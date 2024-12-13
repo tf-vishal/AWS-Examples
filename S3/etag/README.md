38327108b86ddc466fa0e4d69d1dcb62953fafa5
tf.vishal <vvishalsharma0609@gmail.com>
2024-12-13 13:53:33 +0530

    Etag with examples, before changing and after changing

---
+++ S3/etag/README.md
@@ -0,0 +1,3 @@
+# Infrastructure as Code (IaC) doesn't support or provide the object supprot as object doesn't count as an Infrastrcuture in AWS.
+### Terraform gives some support for Object in IaC
+### Cloudformation don't give Object support in IaC
---
+++ S3/etag/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("myfile.txt")
+}
+
+
+#if u wanna download the file u uploaded u can use the command
+# aws s3 cp s3://<bucketname>/<filename> ./path
+#ex- aws s3 cp s3://terraform-20241213073729023800000001/myfile.txt ./myfile.txt 
