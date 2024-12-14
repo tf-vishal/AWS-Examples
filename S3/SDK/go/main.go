2d1f73e63a300f7e79692d78f5da039f987e7878
tf.vishal <vvishalsharma0609@gmail.com>
2024-12-14 18:26:51 +0530

    golang

--- S3/SDK/go/main.go
+++ S3/SDK/go/main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+// Function to create a bucket
+func createBucket(bucketName string, region string) error {
+	// Load the AWS configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+	if err != nil {
+		return fmt.Errorf("failed to load AWS configuration: %w", err)
+	}
+
+	// Create an S3 client
+	s3Client := s3.NewFromConfig(cfg)
+
+	// Create the bucket
+	_, err = s3Client.CreateBucket(context.TODO(), &s3.CreateBucketInput{
+		Bucket: aws.String(bucketName),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create bucket: %w", err)
+	}
+
+	fmt.Printf("Bucket '%s' created successfully in region '%s'.\n", bucketName, region)
+	return nil
+}
+
+func main() {
+	// Specify the bucket name and region
+	bucketName := "example-go-bucket"
+	region := "us-east-1"
+
+	// Call the createBucket function
+	err := createBucket(bucketName, region)
+	if err != nil {
+		log.Fatalf("Error creating bucket: %v", err)
+	}
+}
